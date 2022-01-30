366d9477c1f8b5faf822b210358c8438b91d3388
priyanjali30 <priyanjalich01@gmail.com>
2022-01-30 18:22:40 +0530

    second update

--- routes/files.js
+++ routes/files.js
@@ -6,7 +6,7 @@ const File = require("../models/file");
 const { v4: uuidv4 } = require("uuid");
 
 let storage = multer.diskStorage({
-	destination: (req, file, cb) => cb(null, "uploads/"),
+	destination: (req, file, cb) => cb(null, "uploads"),
 	filename: (req, file, cb) => {
 		const uniqueName = `${Date.now()}-${Math.round(
 			Math.random() * 1e9
